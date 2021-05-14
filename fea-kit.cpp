31c11df89be4fcc395904aaab6aa34e43eb19a90
Johann Scheepers <johann.scheepers.uni@gmail.com>
2021-05-13 22:13:00 -0400

    Added test mesh file in form of LS-DYNA key file and began creating method in mesh object to read and store mesh nodes and element definitions from LS-DYNA mesh files

--- fea-kit.cpp
+++ fea-kit.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
+
+//using namespace std;
 
 class mesh{
     private:
@@ -7,25 +11,66 @@ class mesh{
     std::vector<std::vector<double>> elements;
 
     public:
-    mesh(std::vector<std::vector<double>> nod, std::vector<std::vector<double>> ele){
-        nodes = nod;
-        elements = ele;
+    mesh(std::string fileName){
+        std::vector<std::vector<double>> nodal;
+        std::vector<std::vector<double>> elemental;
+        
+        //Prepare to read mesh text file
+        std::ifstream inFile;
+        std::string x;
+        inFile.open(fileName);
+        if(!inFile){
+            std::cerr<<"Unable to open file!"<<std::endl;
+            exit(1);
+        }
+        //"While values are being read"
+        //Reading and storing mesh and instructions
+        std::string instruction = "";
+        while(inFile >> x){
+            int nodal_track = 0;
+            int elemental_track = 0;
+
+            if(x.front() =='*'){
+                instruction = x;
+            }
+            if(instruction == "*NODE"){
+                std::string n,N_x,N_y,N_z;
+                inFile >> n;
+                inFile >> N_x;
+                inFile >> N_y;
+                inFile >> N_z;            
+                nodal.push_back({std::stod(N_x),std::stod(N_y),std::stod(N_z)});
+
+                nodal_track = nodal_track + 1;
+            }
+            if(instruction == "*ELEMENT_SHELL"){
+                elemental.push_back({});                
+            }
+        }
+        inFile.close();
+
+        nodes = nodal;
+        elements = elemental;
     }
     void printNodes(){
         for(int i =1;i<nodes.size();i++){
             std::cout<<"Node: "<<i<<std::endl;
-            for(int j = 0;j<nodes[i].size();j++){
-                std::cout<<nodes[i][j]<<std::endl;
+            std::cout<<"x: "<<nodes[i][0]<<" y: "<<nodes[i][1]<<" z: "<<nodes[i][2]<<std::endl;
+        }
+    }
+    void printElements(){
+        for(int i=0; i<elements.size();i++){
+            std::cout<<"Element:"<<i<<std::endl;
+            for(int j = 0;j<elements[i].size();j++){
+                std::cout<<elements[i][j]<<std::endl;
             }
         }
     }
 };
 
+
 int main(){
-    std::vector<std::vector<double>> nodal = {{1,2,3},{0,0,0},{1,1,1},{2,2,2}};
-    std::vector<std::vector<double>> elemental = {{1,2},{2,3}};
-    
-    mesh MESH(nodal,elemental);
+    mesh MESH("mesh.txt");
 
     MESH.printNodes();
 
